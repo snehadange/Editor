f56653e4eb8739889ece46c213fc392596aea2e5
snehadange <32454950+snehadange@users.noreply.github.com>
2023-02-18 00:14:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import HandwritingCanvas from './Screen/HandwritingCanvas';
+
+const App = () => {
+  return <HandwritingCanvas />;
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
